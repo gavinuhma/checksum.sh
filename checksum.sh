94c5c97b73ba44da62094f46a58dc596c5ec6a84
Gavin Uhma <gavinuhma@users.noreply.github.com>
2022-10-29 02:42:37 +0000

    switch to using --check

--- checksum.sh
+++ checksum.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 function checksum() {
+  local s
   s=$(curl -fsSL "$1")
   if ! command -v shasum >/dev/null
   then
     shasum() { sha1sum "$@"; }
   fi
-  c=$(printf %s\\n "$s" | shasum | awk '{print $1}')
-  if [ "$c" = "$2" ]
+  if [ ! "$2" ]
   then
-    printf %s\\n "$s"
-  else
-    echo "invalid checksum $c != $2" 1>&2;
+    printf %s\\n "$s" | shasum -a 256 | awk '{print $1}'
+    return 1;
   fi
-  unset s
-  unset c
+  printf %s\\n "$s" | shasum -a 256 --check --status <(printf '%s  -\n' "$2") || { echo "checksum failed" >&2; return 1; }
+  printf %s\\n "$s"
 }
