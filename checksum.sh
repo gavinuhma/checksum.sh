291e606fb29f1e8884ca7da9306a4b13e28fb21e
Gavin Uhma <gavinuhma@users.noreply.github.com>
2022-10-31 22:31:42 -0300

    Use var for hash function

--- checksum.sh
+++ checksum.sh
@@ -3,15 +3,19 @@
 function checksum() {
   local s
   s=$(curl -fsSL "$1")
-  if ! command -v shasum >/dev/null
-  then
-    shasum() { sha1sum "$@"; }
+  local h
+  if command -v shasum >/dev/null ; then
+    h=shasum
+  else
+    h=sha1sum
   fi
-  if [ ! "$2" ]
-  then
-    printf %s\\n "$s" | shasum -a 256 | awk '{print $1}'
+  if [ ! "$2" ] ; then
+    printf %s\\n "$s" | "$h" | awk '{print $1}'
     return 1;
   fi
-  printf %s\\n "$s" | shasum -a 256 --check --status <(printf '%s  -\n' "$2") || { echo "checksum failed" >&2; return 1; }
+  printf %s\\n "$s" | "$h" --check --status <(printf '%s  -\n' "$2") || {
+    echo "checksum failed" >&2;
+    return 1;
+  }
   printf %s\\n "$s"
 }
